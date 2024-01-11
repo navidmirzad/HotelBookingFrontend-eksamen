819e999e8cb809a02bdd0018993cbc1b6a742791
navidmirzad <navi0221@stud.kea.dk>
2024-01-11 12:16:42 +0100

    Post Hotel + modal works fine

---
+++ js/addhotel.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var hotelModal = document.getElementById("addHotel");
+    var postBtn = document.getElementById("postHotel");
+    var closeButton = document.getElementById("closeHotelModal");
+
+    postBtn.onclick = function (event) {
+        event.preventDefault();
+        hotelModal.style.display = "block";
+    }
+
+    closeButton.onclick = function () {
+        hotelModal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == hotelModal) {
+            hotelModal.style.display = "none";
+        }
+    }
+
+    const name = document.getElementById("name");
+    const street = document.getElementById("street")
+    const city = document.getElementById("city")
+    const zipcode = document.getElementById("zipcode")
+    const country = document.getElementById("country")
+    const submitHotel = document.getElementById("submitHotel")
+
+
+    submitHotel.addEventListener("click", async function (event) {
+        event.preventDefault(); // Prevent the form from submitting normally
+
+        const hotel = {
+            name: name.value,
+            street: street.value,
+            city: city.value,
+            zipcode: zipcode.value,
+            country: country.value
+        };
+
+        const postUrl = "http://localhost:8080/addhotel";
+
+        try {
+            const response = await postObjectAsJson(postUrl, hotel, "POST");
+            console.log("jeg poster");
+
+            if (response.ok) {
+                alert("Hotel added!");
+                window.location.href = "index.html"
+            } else {
+                alert("Something went wrong");
+            }
+        } finally {
+            console.log("")
+        }
+    });
+
+
+});
+
+async function postObjectAsJson(url, object, httpVerbum) {
+    const objectAsJsonString = JSON.stringify(object)
+    console.log(objectAsJsonString)
+    const fetchOptions = {
+        method: httpVerbum,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: objectAsJsonString
+    }
+    const response = await fetch(url, fetchOptions)
+    return response
+}
+
+
